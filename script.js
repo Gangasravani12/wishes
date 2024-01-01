192fcfcada39dd1b7362fe1310a47008f0e88c7f
Ganga <132571179+Gangasravani12@users.noreply.github.com>
2024-01-01 13:35:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const wishes = document.getElementById("wishes");
+    const footer = document.querySelector("footer");
+    wishes.style.opacity = "1";
+    footer.style.opacity = "1";
+});
